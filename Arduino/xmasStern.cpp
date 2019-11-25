3b5fb9caa9f21d15721b01e3d02b59b2305d160c
PheliBerlin <44943444+PheliBerlin@users.noreply.github.com>
2019-11-25 15:43:31 +0100

    Add files via upload

--- Arduino/xmasStern.cpp
+++ Arduino/xmasStern.cpp
@@ -1,71 +1,73 @@
 #include "Arduino.h"
+
 #include "xmasStern.h"
 
 
-xmasStern::xmasStern(byte latchPin, byte clockPin, byte dataPin)
-{
-	pinMode(latchPin, OUTPUT);
-	pinMode(clockPin, OUTPUT);
-	pinMode(dataPin, OUTPUT);	
-	
-	_latchPin = latchPin;
-	_clockPin = clockPin;
-	_dataPin = dataPin;
-	
-	// Die einzelnen Pins am Stern deklarieren
-	// Jede Zeile eine Spitze
-	// rot, gelb, grün, weiß, grün, geld (Spitze im Uhreigersinn)
-	byte zaehler = 0;
-	for (byte i = 0; i < 5; i++)
-	{
-		for (byte n = 0; n < 6; n++)
-		{
-			_leds[zaehler][0] = 128 >> i;
-			_leds[zaehler][1] = 128 >> n;
-			zaehler += 1;
-		}
-	}
-	aus();
+xmasStern::xmasStern(byte latchPin, byte clockPin, byte dataPin) {
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+
+  _latchPin = latchPin;
+  _clockPin = clockPin;
+  _dataPin = dataPin;
+
+  // Die einzelnen Pins am Stern deklarieren
+  // Jede Zeile eine Spitze
+  // rot, gelb, grün, weiß, grün, geld (Spitze im Uhreigersinn)
+  byte zaehler = 0;
+  for (byte i = 0; i < 5; i++) {
+    for (byte n = 0; n < 6; n++) {
+      _leds[zaehler][0] = 128 >> i;
+      _leds[zaehler][1] = 128 >> n;
+      zaehler += 1;
+    }
+  }
+  aus();
 }
 
-void xmasStern::shiftOut_neu(byte wert)
+void xmasStern::sternDelay(unsigned long zeit)
 {
+	unsigned long time = millis();
+	unsigned long diff;
+	
+	do
+	{
+		diff = millis() - time;
+	} while ( diff < zeit );
+}
+
+void xmasStern::shiftOut_neu(byte wert) {
   byte i;
 
-  for (i = 0; i < 8; i++)
-  {
-		//digitalWrite(dataPin, !!(val & (1 << i)));
-    if (  ( !!(wert & (1 << i)) ) == HIGH )
+  for (i = 0; i < 8; i++) {
+    //digitalWrite(dataPin, !!(val & (1 << i)));
+    if ((!!(wert & (1 << i))) == HIGH)
       PORTA |= (1 << _dataPin);
     else PORTA &= ~(1 << _dataPin);
 
     //digitalWrite(clockPin, HIGH);
     PORTA |= (1 << _clockPin);
-    
+
     //digitalWrite(clockPin, LOW);
-    PORTA &= ~(1 << _clockPin); 
+    PORTA &= ~(1 << _clockPin);
   }
 }
 
+void xmasStern::senden(byte wert1, byte wert2) {
+  // latchPin auf LOW damit
+  // die LED beim Senden nicht blinken
 
+  //digitalWrite(latchPin, LOW);
+  PORTA &= ~(1 << _latchPin);
 
-void xmasStern::senden(byte wert1, byte wert2)
-{
-	// latchPin auf LOW damit
-	// die LED beim Senden nicht blinken
+  // Erst das zweite Register, dann das erste
+  shiftOut_neu(wert2);
+  shiftOut_neu(wert1);
 
-	//digitalWrite(latchPin, LOW);
-	PORTA &= ~(1 << _latchPin);
-	
-	// Erst das zweite Register, dann das erste
-	shiftOut_neu(wert2);
-	shiftOut_neu(wert1);
-	
-	//digitalWrite(latchPin, HIGH);
-	PORTA |= (1 << _latchPin);	
+  //digitalWrite(latchPin, HIGH);
+  PORTA |= (1 << _latchPin);
 }
-
-
 
 // Nur die einzelnen Farben ansteuren
 void xmasStern::weiss() {
@@ -76,73 +78,58 @@ void xmasStern::rot() {
   senden(0b11111000, 0b10000000);
 }
 
-void xmasStern::gruen()
-{
+void xmasStern::gruen() {
   senden(0b11111000, 0b00101000);
 }
 
-void xmasStern::gelb()
-{
+void xmasStern::gelb() {
   senden(0b11111000, 0b01000100);
 }
 
-
-
 // Alle LED ausschalten
-void xmasStern::aus()
-{
+void xmasStern::aus() {
   senden(0, 0);
 }
 
+// Alle LED ausschalten
+void xmasStern::an() {
+  senden(255, 255);
+}
 
 // Effekte
-void xmasStern::umlauflinks(int speed)
-{
-  byte ic1, ic2;
-
-  for (byte spitze = 1; spitze < 6; spitze++)
-  {
-    ic1 = 256 >> spitze;
-
-    for ( byte lampe = 1; lampe < 7; lampe++)
-    {
-      ic2 = 256 >> lampe;
-      senden(ic1, ic2);
-      delay(speed);
-    }
-  }
+void xmasStern::umlaufLinks(int speed) {
+	for ( byte i = 0; i< 30; i++ )
+	{
+		eineLED(i);
+		sternDelay(speed);
+	}
 }
-void xmasStern::umlaufrechts(int speed)
-{
-  byte ic1, ic2;
-
-  for (byte spitze = 1; spitze < 6; spitze++)
+void xmasStern::umlaufrechts(int speed) {
+  for ( byte i = 30; i > 0; i-- )
   {
-    ic1 = 4 << spitze;
-
-    for ( byte lampe = 1; lampe < 7; lampe++)
-    {
-      ic2 = 2 << lampe;
-      senden(ic1, ic2);
-      delay(speed);
-    }
-  }
+	  eineLED(i);
+	  sternDelay(speed);
+	}
 }
-void xmasStern::sparkle(int speed) // speed = 250-300 sieht gut aus
-{
-  weiss(); delay(speed);
-  gruen(); delay(speed);
-  gelb(); delay(speed);
-  rot(); delay(speed);
-  gelb(); delay(speed);
-  gruen(); delay(speed);
+void xmasStern::sparkle(int speed) {// speed = 250-300 sieht gut aus 
+  weiss();
+  sternDelay(speed);
+  gruen();
+  sternDelay(speed);
+  gelb();
+  sternDelay(speed);
+  rot();
+  sternDelay(speed);
+  gelb();
+  sternDelay(speed);
+  gruen();
+  sternDelay(speed);
 }
 void xmasStern::viererrunde(int speed) // speed = 30-40 sieht gut aus
 {
   byte led1, led2, led3, led4;
 
-  for ( led1 = 0; led1 < 30; led1++)
-  {
+  for (led1 = 0; led1 < 30; led1++) {
     led2 = led1 + 1;
     led2 = (led2 == 30 ? 0 : led2);
     led3 = led1 + 2;
@@ -153,80 +140,122 @@ void xmasStern::viererrunde(int speed) // speed = 30-40 sieht gut aus
     led4 = (led4 == 31 ? 1 : led4);
     led4 = (led4 == 32 ? 2 : led4);
 
-    for (int i = 0; i < speed; i++)
-    {
+    for (int i = 0; i < speed; i++) {
       senden(_leds[led1][0], _leds[led1][1]);
-      //delay(1);
       senden(_leds[led2][0], _leds[led2][1]);
-      //delay(1);
       senden(_leds[led3][0], _leds[led3][1]);
-      //delay(1);
       senden(_leds[led4][0], _leds[led4][1]);
-      //delay(1);
     }
   }
 }
-void xmasStern::gelberring(int runden, int speed)
-{
-  for (byte n = 0; n < runden ; n++)
-  {
+void xmasStern::gelberring(int runden, int speed) {
+  for (byte n = 0; n < runden; n++) {
     senden(0b11111000, 0b01000000);
-    delay(speed);
+    sternDelay(speed);
     senden(0b11111000, 0b00000100);
-    delay(speed);
+    sternDelay(speed);
   }
 }
-void xmasStern::gruenerring(int runden, int speed)
-{
-  for (byte n = 0; n < runden ; n++)
-  {
+void xmasStern::gruenerring(int runden, int speed) {
+  for (byte n = 0; n < runden; n++) {
     senden(0b11111000, 0b00001000);
-    delay(speed);
+    sternDelay(speed);
     senden(0b11111000, 0b00100000);
-    delay(speed);
+    sternDelay(speed);
   }
 }
-void xmasStern::ring(int runden, int speed)
-{
-  for (byte n = 0; n < runden ; n++)
-  {
+void xmasStern::ring(int runden, int speed) {
+  for (byte n = 0; n < runden; n++) {
     senden(0b11111000, 0b11010000);
-    delay(speed);
+    sternDelay(speed);
     senden(0b11111000, 0b10100100);
-    delay(speed);
+    sternDelay(speed);
     senden(0b11111000, 0b01011000);
-    delay(speed);
+    sternDelay(speed);
     senden(0b11111000, 0b00101100);
-    delay(speed);
+    sternDelay(speed);
   }
 }
-void xmasStern::spitze(byte spitze, int dauer)
-{
+void xmasStern::spitze(byte spitze, int dauer) {
   if (spitze > 4) return;
 
-  for ( int i = 0; i < dauer; i++)
-  {
-    senden( 128 >> spitze, 0b01111100);
-    delay(2);
+  for (int i = 0; i < dauer; i++) {
+    senden(128 >> spitze, 0b01111100);
+    sternDelay(2);
     rot();
-    delay(5);
+    sternDelay(4);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void xmasStern::eineLED(byte nummer) {
+  if (nummer > 29) return;
+  senden(_leds[nummer][0], _leds[nummer][1]);
+}
+void xmasStern::eineLEDaus(byte nummer) {
+  if (nummer > 29) return;
+  for (byte i = 0; i < 30; i++) {
+    if (i == nummer) {
+      aus();
+    } else {
+      eineLED(i);
+    }
+  }
+}
+void xmasStern::umlaufAlleRechts(int spped) {
+  for (byte i = 30; i > 0; i--) {
+    for (byte n = 0; n < 20; n++) {
+      eineLEDaus(i - 1);
+    }
+  }
+}
+void xmasStern::umlaufAlleLinks(int speed) {
+  for (byte i = 0; i < 29; i++) {
+    for (byte n = 0; n < 20; n++) {
+      eineLEDaus(i);
+    }
+  }
+}
+void xmasStern::feuerWerk(int speed) {
+	int i = speed * 2;
+	
+	rot();
+  sternDelay(i);
+  gelb();
+  sternDelay(i);
+  gruen();
+  sternDelay(i);
+  an();
+	sternDelay(i);
+  weiss();
+  sternDelay(speed);
+  aus();
+  sternDelay(speed);
+  weiss();
+  sternDelay(speed);
+  aus();
+  sternDelay(speed);
+  weiss();
+  sternDelay(speed);
+  aus();
+  sternDelay(speed);
+	weiss();
+  sternDelay(speed);
+  aus();
+  sternDelay(speed);
+}
+void xmasStern::tickTack(byte spitze1, byte spitze2, byte speed) {
+	spitze(spitze1, speed);
+	spitze(spitze2, speed);
+	spitze(spitze1, speed);
+	spitze(spitze2, speed);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- Arduino/xmasStern.h
+++ Arduino/xmasStern.h
@@ -8,6 +8,9 @@ class xmasStern
 	public:
 		xmasStern(byte latchPin, byte clockPin, byte dataPin);
  
+		// Arduino delay ersetzen
+		void sternDelay(unsigned long zeit);
+		
 		// Nur die einzelnen Farben ansteuren
 		void weiss();
 		void rot();
@@ -15,7 +18,7 @@ class xmasStern
 		void gelb();
 		
 		// Effekte	
-		void umlauflinks(int speed);
+		void umlaufLinks(int speed);
 		void umlaufrechts(int speed);
 		void sparkle(int speed);
 		void viererrunde(int speed);
@@ -23,15 +26,26 @@ class xmasStern
 		void gruenerring(int runden, int speed);
 		void ring(int runden, int speed);
 		void spitze(byte spitze, int dauer);
+		void eineLED(byte nummer);
+		void eineLEDaus(byte nummer);
+		void umlaufAlleRechts(int speed);
+		void umlaufAlleLinks(int speed);
+		void feuerWerk(int speed);
+		void tickTack(byte spitze1, byte spitze2, byte speed);
+		
 		
 		// Alle LED ausschalten
 		void aus();
+		
+		// Alle LED anschalten
+		void an();
         
 	private:
 		byte _latchPin, _clockPin, _dataPin;
 		byte _leds[30][2];
 		void senden(byte wert1, byte wert2);
 		void shiftOut_neu(byte wert);
+		
                 
 };
 
